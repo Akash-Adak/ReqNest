4a499d65deaae4b178cc514ece51a6f6243bcdf8
Akash Adak <akashadak00023@gmail.com>
2025-09-01 00:39:48 +0530

    add some getter setter on email

--- backend-engine/src/main/java/com/akash_adak/backend_engine/notification/EmailRequest.java
+++ backend-engine/src/main/java/com/akash_adak/backend_engine/notification/EmailRequest.java
@@ -13,4 +13,52 @@ public class EmailRequest {
     private boolean html;
     private String type; // e.g. "PAYMENT", "WELCOME", "ALERT"
     private List<MultipartFile> attachments;
+
+    public List<String> getTo() {
+        return to;
+    }
+
+    public void setTo(List<String> to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public boolean isHtml() {
+        return html;
+    }
+
+    public void setHtml(boolean html) {
+        this.html = html;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public List<MultipartFile> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(List<MultipartFile> attachments) {
+        this.attachments = attachments;
+    }
 }
