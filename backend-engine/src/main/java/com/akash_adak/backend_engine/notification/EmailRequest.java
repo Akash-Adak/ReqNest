ac0569956f9d4b55eef8f306470b96767f7bb607
Akash Adak <akashadak00023@gmail.com>
2025-09-02 01:00:02 +0530

    add getter setter in emailrequest model

--- backend-engine/src/main/java/com/akash_adak/backend_engine/notification/EmailRequest.java
+++ backend-engine/src/main/java/com/akash_adak/backend_engine/notification/EmailRequest.java
@@ -13,6 +13,60 @@ public class EmailRequest {
     private String amount;
     private String validUntil;
 
-    // Getters & Setters
-    // ...
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(String validUntil) {
+        this.validUntil = validUntil;
+    }
+
 }
