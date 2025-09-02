ef25e567bd991768aa1e9cb5f9efb09f874880de
Akash Adak <akashadak00023@gmail.com>
2025-09-02 09:54:29 +0530

    .

--- backend-engine/src/test/java/com/akash_adak/backend_engine/BackendEngineApplicationTests.java
+++ backend-engine/src/test/java/com/akash_adak/backend_engine/BackendEngineApplicationTests.java
@@ -2,8 +2,9 @@ package com.akash_adak.backend_engine;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendEngineApplicationTests {
 
